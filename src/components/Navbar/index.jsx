6f7c1fe6c871bd836a069d7f144af6a8a20f864c
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-22 19:38:15 -0300

    zerando estilos

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+// import User from '../../imagens/icons/user.png'
 
 function NavBar() {
     return (
         <div>
             <h1>Finestra Eyewear</h1>
-            <a><img href=""></img></a>
+            <a><img src='../../imagens/icons/search.png'></img></a>
+            <a><img src='../../imagens/icons/user.png'></img></a>
+            <a><img src='../../imagens/icons/shopping.png'></img></a>
         </div>
     )
 }
