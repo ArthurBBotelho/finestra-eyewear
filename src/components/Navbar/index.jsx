b3e70922a0df89fb2d6c745e2601facb7c1746c3
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-22 19:51:11 -0300

    incluindo tailwind

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
     return (
         <div>
-            <h1>Finestra Eyewear</h1>
+            <h1 class="text-3xl font-bold underline">Finestra Eyewear</h1>
             <a><img src='../../imagens/icons/search.png'></img></a>
             <a><img src='../../imagens/icons/user.png'></img></a>
             <a><img src='../../imagens/icons/shopping.png'></img></a>
