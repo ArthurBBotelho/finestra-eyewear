a16c38b4c5976763695d386bdec4087d3931c303
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-23 19:34:19 -0300

    adicionado extenção jsx no tailwind

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,12 +7,13 @@ import search from '../../imagens/icons/search.svg'
 function NavBar() {
     return (
         <div>
-            <header class="bg-grey">
-                <h1 class="text-xl font-medium text-slate-500">Finestra Eyewear</h1>
+            <header class="flex">
+                <h1 class="text-3xl font-bold bg-slate-600">Finestra Eyewear</h1>
                 <a><img width={20} src={search} alt="Icone de Busca" ></img></a>
                 <a><img width={20} src={user} alt="Icone do Usuário" ></img></a>
                 <a><img width={20} src={cart} alt="Carrinho de Compras" ></img></a>
             </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nihil, alias ab adipisci a eveniet, numquam animi unde repudiandae facilis doloribus sit expedita error ratione, quis quam quibusdam perspiciatis. Similique.</p>
         </div>
     )
 }
