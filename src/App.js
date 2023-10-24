71cc8a00cc60815de614f006c51e5997fcb18131
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-24 19:18:43 -0300

    imagem comentada, dando erro na importação

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './index.css'
 import NavBar from './components/NavBar'
 import Button from './components/Button'
 
-import ImgAmarela from './imagens/photos/katsiaryna-endruszkiewicz-BteCp6aq4GI-unsplash.jpg'
+// import ImgAmarela from './imagens/photos/katsiaryna-endruszkiewicz-BteCp6aq4GI-unsplash.jpg'
 
 function App() {
   return (
     <div>
       <NavBar />
 
-      
-        <ImgAmarela />
+{/*       
+        <ImgAmarela /> */}
       
       <Button />
     </div>
