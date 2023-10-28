83fc311ae169195b905f5d92ef5e5b27ea5350e0
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-28 20:16:38 -0300

    Criado o componente ItemCount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './index.css'
 import NavBar from './components/NavBar'
 import Button from './components/Button'
+import Oculos from './imagens/photos/katsiaryna-endruszkiewicz-BteCp6aq4GI-unsplash.jpg';
 
 function App() {
   return (
-    <div>
+    <div className='w-full'>
       <NavBar />
-      <img src="./imagens/photos/katsiaryna-endruszkiewicz-BteCp6aq4GI-unsplash.jpg"></img>
-      
-      <Button />
+      <div className="flex mt-12 w-4/5 mx-auto">
+        <div>
+          <img src={Oculos}></img>
+        </div>
+        <div>
+          <img src={Oculos}></img>
+          <Button />
+        </div>
+      </div> 
+    
     </div>
   );
 }
