6f6df79f7ec9fde4f0b0bfe1bb2c66e89e5b0ce1
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-28 20:59:25 -0300

    criada pasta de produtos com imagens

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import NavBar from './components/NavBar'
 import Button from './components/Button'
 import OculosAmarelo from './imagens/photos/oculos-amarelo.jpg'
 import OculosFlor from './imagens/photos/oculos-flor.jpg'
+import Products from './Products'
 
 function App() {
   return (
-    <div className='w-full'>
+    <div class='w-full'>
       <NavBar />
-      <div className="flex mt-12 w-4/5 mx-auto">
+      <div class="flex mt-12 w-5/6 mx-auto gap-12">
         <div>
-          <img src={OculosAmarelo}></img>
+          <img class="" src={OculosFlor}></img>
         </div>
-        <div>
-          <img src={OculosFlor}></img>
+        <div class="mt-8">
+          <img src={OculosAmarelo}></img>
           <Button />
         </div>
       </div> 
-    
+      <div>
+        <h1>Produtos</h1>
+        <Products />
+      </div>
     </div>
   );
 }
---
+++ src/Products/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Phinet from '../imagens/products/phinet/phinet-front.png'
+import Button from "../components/Button";
+
+function Products() {
+    return(
+        <div class="">
+            <a><img class="w-1/2" src={Phinet} alt="Oculos de Grau Phinet"></img></a>
+            
+            <Button />
+        </div>
+    )
+}
+
+export default Products
