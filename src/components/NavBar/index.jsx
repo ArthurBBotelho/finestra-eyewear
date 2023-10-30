6392fb6daf40336336c2fe794f24f4881cd87539
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-30 19:18:22 -0300

    NavBar maiusculo

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CartWidget from '../CartWidget';
+import SearchWidget from '../SearchWidget';
+import UserWidget from '../UserWidget';
+
+function Navbar() {
+    return (
+        <div>
+            <header class="p-6 grid grid-cols-2 w-auto h-22">
+                <div class="flex align-bottom">
+                    <h1 class="px-12 font-bold text-3xl ">Finestra Eyewear</h1>
+                    <a class="px-12 font-thin text-xl">Produtos</a>
+                    <a class="pr-12 font-thin text-xl">Sobre</a>
+                </div>
+                <div class="flex justify-end">
+                    <SearchWidget />
+                    <UserWidget />
+                    <CartWidget />
+                </div>
+            </header>
+            <div class="w-auto h-0.5 bg-black"></div>
+        </div>
+    )
+}
+
+export default Navbar
