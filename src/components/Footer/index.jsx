716d9c14c5c0971fa836472abb2227a206d8d6d6
Arthur Botelho <emilioarthur19@gmail.com>
2023-11-04 16:13:57 -0300

    footer adiconado e estilizado com sucesso

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,27 @@
+import { ReactComponent as Instagram } from '../../imagens/icons/instagram.svg';
+import { ReactComponent as Facebook } from '../../imagens/icons/facebook.svg';
+import { ReactComponent as Twitter } from '../../imagens/icons/twitter.svg';
+import { ReactComponent as Linkedin } from '../../imagens/icons/linkedin.svg';
 
 function Footer () {
     return(
-        <div className="bg-black text-white w-full h-64 px-12 py-6">
-            <div className="font-bold text-8xl">
+        <div className="bg-black flex">
+            <div className="font-bold text-8xl text-white w-full h-64 px-12 py-6">
                 <p>FINESTRA</p>
                 <p>EYEWEAR</p>
             </div>
-            <div>
-                <p>Finestra Eyewear, Produção e Comércio de Artesanatos CNPJ 13.542.297/0001-00</p>
+            <div className="px-12 pt-12 mt-6">
+                <div className="flex">
+                    <Instagram className="h-16 w-18"/>
+                    <Facebook className="h-16 w-18"/>
+                    <Twitter className="h-16 w-18"/>
+                    <Linkedin className="h-16 w-18"/>
+                </div>
+                <div className="bg-white h-1 w-auto"></div>
+                <div className="text-white">
+                    <p>© Finestra Eyewear, Produção e Comércio de Artesanatos CNPJ 13.542.297/0001-00</p>
+                    <p>Website desenvolvido por Arthur Botelho 2023 </p>
+                </div>
             </div>
         </div>
     )
